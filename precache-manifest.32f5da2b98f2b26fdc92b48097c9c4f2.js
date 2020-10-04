457efb3fb6fb800f0e3a3db7a46e723da246adcc
Muhammad-Bilal-7896 <bilalmohib7896@gmail.com>
2020-10-04 07:38:05 +0500

    FRP

---
+++ precache-manifest.32f5da2b98f2b26fdc92b48097c9c4f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24295bbc6bcdd83e90c91dea3c55e78b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7a9e2e35b6ceaeedb2a",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "917c2f7c98ff14a021a0",
+    "url": "/static/css/main.c9068132.chunk.css"
+  },
+  {
+    "revision": "e7a9e2e35b6ceaeedb2a",
+    "url": "/static/js/2.2f400684.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.2f400684.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "917c2f7c98ff14a021a0",
+    "url": "/static/js/main.810199c5.chunk.js"
+  },
+  {
+    "revision": "64c1e5fce6f564a0eb92",
+    "url": "/static/js/runtime-main.114859f1.js"
+  }
+]);
